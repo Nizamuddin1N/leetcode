07c0490f77ed0cdef29b165de8f0d5c4449abcda
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-07-29 13:35:17 +0530

    Time: 1570 ms (5.04%), Space: 11.7 MB (88.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int ans=0;
-    void recursive(vector<int>&nums, int target, int sum, int index){
+    void recursive(vector<int>&nums, int target, int &sum, int index){
         if(index == nums.size()){
             if(sum == target){
                 ans++;
             }
             return;
         }
-        recursive(nums, target, sum+nums[index], index+1);
-        recursive(nums, target, sum-nums[index], index+1);
+        sum +=nums[index];
+        recursive(nums, target, sum, index+1);
+        sum -=nums[index];
+
+        sum -=nums[index];
+        recursive(nums, target, sum, index+1);
+        sum +=nums[index];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        recursive(nums, target, 0, 0);
+        int sum=0;
+        recursive(nums, target, sum, 0);
         return ans;
     }
 };
