7221056e309a88978e26c3dd5b426108992009ea
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-07-29 19:14:38 +0530

    Time: 0 ms (100%), Space: 10.5 MB (50.64%) - LeetHub

---
+++ 0553-optimal-division/0553-optimal-division.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return to_string(nums[0]);
+        }
+        if (nums.size() == 2) {
+            return to_string(nums[0]) + "/" + to_string(nums[1]);
+        }
+        string ans = "";
+        ans += to_string(nums[0]);
+        ans += "/(";
+        for (int i = 1; i < nums.size(); i++) {
+            ans += to_string(nums[i]);
+            if (i != nums.size() - 1) {
+                ans += "/";
+            }
+        }
+        ans += ")";
+        return ans;
+    }
+};
