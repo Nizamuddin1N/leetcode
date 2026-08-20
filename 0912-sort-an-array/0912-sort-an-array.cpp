664bd70b19dd099ab0930ed975c259cb2cad8606
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-08-20 17:54:55 +0530

    Time: 615 ms (34.94%), Space: 295.7 MB (24.07%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void merge(vector<int>&nums, int low, int mid, int high){
+        vector<int>ans;
+        int i=low;
+        int j=mid+1;
+        while(i<=mid && j<=high){
+            if(nums[i]<=nums[j]){
+                ans.push_back(nums[i]);
+                i++;
+            }
+            else{
+                ans.push_back(nums[j]);
+                j++;
+            }
+        }
+        while(i<=mid){
+            ans.push_back(nums[i]);
+            i++;
+        }
+        while(j<=high){
+            ans.push_back(nums[j]);
+            j++;
+        }
+        int k=0;
+        for(int x=low; x<=high; x++){
+            nums[x] = ans[k];
+            k++;
+        }
+    }
+    void mergsort(vector<int>&nums, int low, int high){
+        if(low<high){
+            int mid = low + (high-low)/2;
+            mergsort(nums, low, mid);
+            mergsort(nums, mid+1, high);
+            merge(nums, low, mid, high);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergsort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
