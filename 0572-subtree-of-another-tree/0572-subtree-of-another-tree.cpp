5ccdb2e583e2700d08c67690c3a6dd44c9556f0f
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-08-22 13:12:51 +0530

    Time: 11 ms (5.2%), Space: 35.4 MB (5.05%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+
+    bool checkSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root == nullptr && subRoot == nullptr)
+            return true;
+        if(root == nullptr || subRoot == nullptr)
+            return false;
+        if(root->val != subRoot->val)
+            return false;
+        queue<pair<TreeNode*, TreeNode*>> q;
+        q.push({root, subRoot});
+
+        while(!q.empty()) {
+            auto [node1, node2] = q.front();
+            q.pop();
+            if(node1->val != node2->val)
+                return false;
+            if(node1->left == nullptr && node2->left != nullptr)
+                return false;
+            if(node1->left != nullptr && node2->left == nullptr)
+                return false;
+            if(node1->right == nullptr && node2->right != nullptr)
+                return false;
+            if(node1->right != nullptr && node2->right == nullptr)
+                return false;
+            if(node1->left && node2->left)
+                q.push({node1->left, node2->left});
+            if(node1->right && node2->right)
+                q.push({node1->right, node2->right});
+        }
+
+        return true;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+        if(subRoot == nullptr)
+            return true;
+        if(root == nullptr)
+            return false;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if(node->val == subRoot->val) {
+
+                if(checkSubtree(node, subRoot))
+                    return true;
+            }
+
+            if(node->left)
+                q.push(node->left);
+
+            if(node->right)
+                q.push(node->right);
+        }
+
+        return false;
+    }
+};
