6274a507902af9c57a4c10d03efa3f575c13b8ef
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-08-26 20:44:13 +0530

    Time: 0 ms (100%), Space: 16.6 MB (17.87%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root, unordered_set<TreeNode*> &st){
+        if(root == nullptr){
+            return nullptr;
+        }
+        if(st.find(root) != st.end()){
+            return root;
+        }
+        TreeNode* leftAns = solve(root->left, st);
+        TreeNode* rightAns = solve(root->right, st);
+        if(leftAns != nullptr && rightAns != nullptr){
+            return root;
+        }
+        else if(leftAns != nullptr && rightAns == nullptr){
+            return leftAns;
+        }
+        else if(leftAns == nullptr && rightAns != nullptr){
+            return rightAns;
+        }
+        else{
+            return nullptr;
+        }
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        unordered_set<TreeNode*>st;
+        queue<TreeNode*>q;
+        if(root==NULL){
+            return NULL;
+        }
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            st.clear();
+            for(int i=0; i<n; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                st.insert(node);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+        }
+        return solve(root, st);
+    }
+};
