6b124966fbed3320757bd8b9c1fc5fa16b98f0e7
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-09-15 14:49:20 +0530

    Time: 0 ms (100%), Space: 24.3 MB (81.65%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -11,27 +11,57 @@
  */
 class Solution {
 public:
-    unordered_map<int, int>mp;
-    void storeFrequency(TreeNode* root){
-        if(root == nullptr){
+    int currcount=0;
+    int maxcount=0;
+    int prev=0;
+    bool hasprev = false;
+    vector<int>ans;
+    void solve(TreeNode* root){
+        if(root==nullptr){
             return;
         }
-        mp[root->val]++;
-        storeFrequency(root->left);
-        storeFrequency(root->right);
+        solve(root->left);
+        if(hasprev && prev == root->val){
+            currcount++;
+        }
+        else{
+            currcount=1;
+            hasprev = true;
+        }
+        if(currcount>maxcount){
+            maxcount = currcount;
+            ans.clear();
+            ans.push_back(root->val);
+        }
+        else if(currcount==maxcount){
+            ans.push_back(root->val);
+        }
+        prev = root->val;
+        solve(root->right);
     }
+    // unordered_map<int, int>mp;
+    // void storeFrequency(TreeNode* root){
+    //     if(root == nullptr){
+    //         return;
+    //     }
+    //     mp[root->val]++;
+    //     storeFrequency(root->left);
+    //     storeFrequency(root->right);
+    // }
     vector<int> findMode(TreeNode* root) {
-        int maxFrequency=INT_MIN;
-        storeFrequency(root);
-        for(auto &e:mp){
-            maxFrequency = max(maxFrequency, e.second);
-        }
-        vector<int>ans;
-        for(auto &e:mp){
-            if(e.second == maxFrequency){
-                ans.push_back(e.first);
-            }
-        }
+        // int maxFrequency=INT_MIN;
+        // storeFrequency(root);
+        // for(auto &e:mp){
+        //     maxFrequency = max(maxFrequency, e.second);
+        // }
+        // vector<int>ans;
+        // for(auto &e:mp){
+        //     if(e.second == maxFrequency){
+        //         ans.push_back(e.first);
+        //     }
+        // }
+        // return ans;
+        solve(root);
         return ans;
     }
 };
