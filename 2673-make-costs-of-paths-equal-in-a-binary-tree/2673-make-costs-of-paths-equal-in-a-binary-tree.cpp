769f68e3dbb9abdb8dee93b7b8f220e95979acf0
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-09-15 18:12:37 +0530

    Time: 15 ms (20.63%), Space: 140.6 MB (32.14%) - LeetHub

--- 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -1,42 +1,53 @@
 class Solution {
 public:
-    vector<int> cost;
-    int m;
-    int ans = 0;
+    // vector<int> cost;
+    // int m;
+    // int ans = 0;
 
-    TreeNode* makebinarytree(int i) {
-        if (i > m)
-            return nullptr;
+    // TreeNode* makebinarytree(int i) {
+    //     if (i > m)
+    //         return nullptr;
 
-        TreeNode* root = new TreeNode(i);
+    //     TreeNode* root = new TreeNode(i);
 
-        root->left = makebinarytree(2 * i);
-        root->right = makebinarytree(2 * i + 1);
+    //     root->left = makebinarytree(2 * i);
+    //     root->right = makebinarytree(2 * i + 1);
 
-        return root;
-    }
+    //     return root;
+    // }
 
-    int store(TreeNode* root) {
-        if (root == nullptr)
+    // int store(TreeNode* root) {
+    //     if (root == nullptr)
+    //         return 0;
+
+    //     int left = store(root->left);
+    //     int right = store(root->right);
+
+    //     ans += abs(left - right);
+
+    //     return cost[root->val - 1] + max(left, right);
+    // }
+    int ans=0;
+    int solution(int i, vector<int>&cost){
+        if(i>cost.size()){
             return 0;
-
-        int left = store(root->left);
-        int right = store(root->right);
-
-        ans += abs(left - right);
-
-        return cost[root->val - 1] + max(left, right);
+        }
+        int left = solution(2*i, cost);
+        int right = solution(2*i+1, cost);
+        ans +=abs(left-right);
+        return cost[i-1]+max(left, right);
     }
-
     int minIncrements(int n, vector<int>& cost) {
-        this->cost = cost;
-        m = n;
-        ans = 0;
+        // this->cost = cost;
+        // m = n;
+        // ans = 0;
 
-        TreeNode* root = makebinarytree(1);
+        // TreeNode* root = makebinarytree(1);
 
-        store(root);
+        // store(root);
 
+        // return ans;
+        solution(1, cost);
         return ans;
     }
 };
