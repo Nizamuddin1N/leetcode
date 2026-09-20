b0a78c2fc45ae15e79ac076699187a336852668a
Nizamuddin <159190139+Nizamuddin1N@users.noreply.github.com>
2026-09-20 15:44:21 +0530

    Time: 0 ms (100%), Space: 54.3 MB (92.62%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> findDegrees(vector<vector<int>>& matrix) {
         int n = matrix.size();
         vector<int>ans(n, 0);
-        unordered_map<int, list<int>>adjList;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 1){
